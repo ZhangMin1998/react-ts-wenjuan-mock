35ef634b79e7557678e9614391533da56260ed9c
ZhangMin1998 <1464253423@qq.com>
2023-12-06 22:33:30 +0800

    数据修改

--- mock/question.js
+++ mock/question.js
@@ -91,6 +91,23 @@ module.exports = [
                 ],
                 value: ''
               }
+            },
+            // Checkbox
+            {
+              fe_id: Random.id(),
+              type: 'questionCheckbox',
+              title: '多选',
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: '多选标题',
+                isVertical: false,
+                list: [
+                  { value: 'item1', text: '选项1', checked: false },
+                  { value: 'item2', text: '选项2', checked: false },
+                  { value: 'item3', text: '选项3', checked: false }
+                ]
+              }
             }
           ]
         }
